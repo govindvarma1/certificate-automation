e7b0531c175b3d7be87bd39af6b44a48193996ed
Govinda Varma <98372421+govindvarma1@users.noreply.github.com>
2024-06-05 15:19:07 +0530

    name changed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,13 +4,13 @@ import { useNavigate } from "react-router-dom";
 export default function Header() {
     const navigate = useNavigate();
     return (
-        <header className="fixed top-0 left-0 w-full bg-gradient-to-r from-blue-600 to-indigo-600 text-white px-4 py-4 flex justify-between items-center z-10 shadow-lg">
-            <h1 className="text-xl font-bold">TuteDude</h1>
+        <header className="fixed top-0 left-0 z-10 flex items-center justify-between w-full px-4 py-4 text-white shadow-lg bg-gradient-to-r from-blue-600 to-indigo-600">
+            <h1 className="text-xl font-bold">CertiCreate</h1>
             <nav>
-                <button onClick={() => {navigate("/")}} className="text-white hover:text-gray-200 transition duration-300 ease-in-out mr-4 mb-1">
+                <button onClick={() => {navigate("/")}} className="mb-1 mr-4 text-white transition duration-300 ease-in-out hover:text-gray-200">
                     Create Cretificate
                 </button>
-                <button onClick={() => {navigate("/view")}} className="text-white hover:text-gray-200 transition duration-300 ease-in-out mb-1">
+                <button onClick={() => {navigate("/view")}} className="mb-1 text-white transition duration-300 ease-in-out hover:text-gray-200">
                     View Certificates
                 </button>
             </nav>
